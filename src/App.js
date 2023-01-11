1fc99b056f7c62dfaadea2300af668c1c6c29099
anish <aanishmv@gmail.com>
2023-01-12 03:28:25 +0530

    hot_fix

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Captcha from "./components/Captcha/Captcha";
 import useGenerateCaptcha from "./hooks/useGenerateCaptcha";
 function App() {
   const [input, setInput] = useState("");
+  const [validateCpatcha, setValidateCaptcha] = useState();
+  const [success, setSuccess] = useState(false);
   const { captcha, generate, match } = useGenerateCaptcha(input);
   useEffect(() => {
     generate(6);
   }, []);
-  const isDisabled = captcha !== input;
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (match) {
+      setValidateCaptcha(true);
+      setSuccess(true);
+    } else {
+      setValidateCaptcha(false);
+    }
+    setInput(" ");
+    generate(6);
+  }
   return (
     <div className="app__container">
       <div className="form__wrapper">
@@ -21,7 +33,7 @@ function App() {
           src="https://softr-prod.imgix.net/applications/9d5e7a64-e0eb-4200-887e-f642ba902626/assets/cf0a1430-49e1-4871-96a4-5fe7ac8617c8.png"
           alt="logo"
         />
-        <form spellCheck={false}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <input autoComplete="off" required type="email" placeholder="Email" />
           <input
             autoComplete="off"
@@ -39,12 +51,12 @@ function App() {
 
           <input
             style={{
-              border: input
-                ? match
-                  ? "2px solid green"
-                  : "2px solid red"
-                : "",
+              border:
+                validateCpatcha !== undefined
+                  ? !validateCpatcha && "2px solid red"
+                  : "",
             }}
+            value={input}
             required
             className="captcha__input"
             type="text"
@@ -52,13 +64,23 @@ function App() {
             placeholder="Enter the captcha here"
           />
 
+          {validateCpatcha !== undefined
+            ? !validateCpatcha && (
+                <span style={{ marginTop: "-15px", fontSize: "0.625rem" }}>
+                  Invalid captcha
+                </span>
+              )
+            : ""}
+
           <button
-            onClick={() => console.log("clicked")}
-            disabled={isDisabled}
+            // disabled={isDisabled}
             className="signup__btn"
           >
             Sign-up
           </button>
+          {validateCpatcha && (
+            <p className="success__message">Successfully registered!!!</p>
+          )}
         </form>
       </div>
     </div>
